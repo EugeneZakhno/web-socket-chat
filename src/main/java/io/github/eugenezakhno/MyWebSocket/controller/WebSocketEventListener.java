d7e1e76aaceec798b08896179ed028dfe2459e02
EugeneZakhno <zohan1234567@gmail.com>
2019-11-12 22:35:23 +0200

    added correct logger

--- src/main/java/io/github/eugenezakhno/MyWebSocket/controller/WebSocketEventListener.java
+++ src/main/java/io/github/eugenezakhno/MyWebSocket/controller/WebSocketEventListener.java
@@ -1,7 +1,9 @@
 package io.github.eugenezakhno.MyWebSocket.controller;
 
 import io.github.eugenezakhno.MyWebSocket.model.ChatMessage;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
@@ -9,18 +11,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
-import java.util.logging.Logger;
-
 @Component
 public class WebSocketEventListener {
 
-    private static final Logger logger = (Logger) LoggerFactory.getLogger(WebSocketEventListener.class);
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
 
-    private final SimpMessageSendingOperations messagingTemplate;
-
-    public WebSocketEventListener(SimpMessageSendingOperations messagingTemplate) {
-        this.messagingTemplate = messagingTemplate;
-    }
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
 
     @EventListener
     public void handleWebSocketConnectListener(SessionConnectedEvent event) {
